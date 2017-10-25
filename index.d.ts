4694ca2ada0a4c1c11436bed7742010c51d5de44
tjoskar <kontakta@oskarkarlsson.nu>
2017-10-25 12:46:48 +0200

    ✨

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+export * from './sns-message';
+export * from './thetvdb';
---
+++ thetvdb/index.d.ts
@@ -0,0 +1,4 @@
+export { TheTvDbEpisode } from './the-tv-db-episode';
+export { TheTvDbShow } from './the-tv-db-show';
+export { TheTvDbShowEpisode, TheTvDbShowEpisodePage } from './the-tv-db-show-episode';
+export { TheTvDbShowImage } from './the-tv-db-show-image';
