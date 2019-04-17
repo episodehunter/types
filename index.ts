c48b0d83d94f1fc41ada16058af9c0ab9d09a34f
tjoskar <kontakta@oskarkarlsson.nu>
2019-04-17 08:40:37 +0200

    ✨

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import * as Message from './message';
 import * as Dragonstone from './dragonstone';
 
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
 export { Message, Dragonstone };
--- message/dragonstone/index.ts
+++ message/dragonstone/index.ts
@@ -1,5 +1,6 @@
 import * as UpdateEpisodes from './update-episodes';
 import * as UpdateShow from './update-show';
 import * as AddShow from './add-show';
+import { ShowInput } from './show-input';
 
-export { UpdateEpisodes, UpdateShow, AddShow };
+export { UpdateEpisodes, UpdateShow, AddShow, ShowInput };
