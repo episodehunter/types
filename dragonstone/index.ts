636462698885523d85bf5df45b59c22f71687caa
tjoskar <kontakta@oskarkarlsson.nu>
2019-05-07 19:12:22 +0200

    ✨

--- dragonstone/index.ts
+++ dragonstone/index.ts
@@ -3,7 +3,8 @@ import { History } from './history';
 import { Show } from './show';
 import { Title } from './title';
 import { UpcomingEpisode } from './upcoming-episode';
+import { User } from './user';
 import * as WatchedEpisode from './watched-episode';
 import { WhatToWatch } from './what-to-watch';
 
-export { Episode, History, Show, Title, UpcomingEpisode, WatchedEpisode, WhatToWatch };
+export { Episode, History, Show, Title, UpcomingEpisode, WatchedEpisode, WhatToWatch, User };
